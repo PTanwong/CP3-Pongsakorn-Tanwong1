a04326702f1a4443e84b9d4c79939ace46923cf5
Frame <ponsakornframe@gmail.com>
2026-06-19 17:18:24 +0700

    ex.5

---
+++ assignments/Exercise5_Pongsakorn_T .py
@@ -0,0 +1,3 @@
+Distance = int(input("Enter Distance: "))
+Time = int(input("Enter time: "))
+print(f"Velocity = {Distance/Time} km/h")
---
+++ assignments/lacture32.py
@@ -0,0 +1,3 @@
+num1 = int(input("Input Num1:"))
+num2 = int(input("Input Num2:"))
+print(f"Result: {num1+num2}")
